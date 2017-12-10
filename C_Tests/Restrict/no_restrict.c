983650303b089c9f2f4ca55a7043b1bcc23bad45
Horia Opris <horia.opris@outlook.com>
2017-12-10 21:24:57 +0200

    Add restrict test

--- C_Tests/Restrict/no_restrict.c
+++ C_Tests/Restrict/no_restrict.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+void initTwoVars(int *a, int *b, int *c)
+{
+    *a += *c;
+    *b += *c;
+}
+
 int main(void)
 {
-    printf(" Does not Works\n");
+    int x = 1;
+    int y = 5;
+    int z = 10;
+
+    initTwoVars(&z, &y, &z);
+
+    /*
+    Expected vs actual result
+    x = 1   |   1
+    y = 25  |   25 
+    z = 20  |   20
+    */
+    printf("Result\n"
+           "x:%d\n"
+           "y:%d\n"
+           "z:%d\n",
+           x, y, z);
 
     return 0;
 }
--- C_Tests/Restrict/restrict.c
+++ C_Tests/Restrict/restrict.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+void initTwoVars(int *restrict a, int *restrict b, int *restrict c)
+{
+    *a += *c;
+    *b += *c;
+}
+
 int main(void)
 {
-    printf("Works\n");
- 
+    int x = 1;
+    int y = 5;
+    int z = 10;
+
+    initTwoVars(&z, &y, &z);
+
+    /*
+    Expected vs actual result
+    x = 1   |   1
+    y = 25  |   25 
+    z = 20  |   20
+    */
+    printf("Result\n"
+           "x:%d\n"
+           "y:%d\n"
+           "z:%d\n",
+           x, y, z);
+
     return 0;
 }
