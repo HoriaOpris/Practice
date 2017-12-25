469ed9da6750149abbe1c16fb256fdce0d0aeaf4
horia <horia@DESKTOP-5KRBMPI>
2017-12-25 21:05:55 +0000

    Add P14 project

---
+++ C_Tests/P14/main.c
@@ -0,0 +1,14 @@
+/* 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 21000?
+ *
+ *
+ * */
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Test \n");
+
+	return 0;
+}
