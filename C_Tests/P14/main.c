8f416c08650427e4e7667bf404f4190df6a05548
horia <horia@DESKTOP-5KRBMPI>
2017-12-25 21:18:25 +0000

    Implement power of two function

--- C_Tests/P14/main.c
+++ C_Tests/P14/main.c
@@ -1,14 +1,22 @@
-/* 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+/* 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
  * What is the sum of the digits of the number 21000?
  *
  *
  * */
 
 #include<stdio.h>
+#include<math.h>
+
+unsigned int getTwoToThePow(unsigned int power);
 
 int main(void)
 {
-	printf("Test \n");
+	printf("Test:%d \n", getTwoToThePow(15));
 
 	return 0;
+}
+
+unsigned int getTwoToThePow(unsigned int power)
+{
+	return pow(2,power);
 }
