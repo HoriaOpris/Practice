24542ec067cfd76dca729f9136758b883b0639d6
Horia Opris <horia.opris@outlook.com>
2018-08-12 16:59:08 +0100

    Implement Intel Timer read

--- OS/Preemptive/main.c
+++ OS/Preemptive/main.c
@@ -6,16 +6,31 @@
 
 #include <stdio.h>
 
+static inline volatile unsigned long long RDTSC(void);
 static void Task_one(void);
 static void Task_two(void);
 
 void main(void)
 {
-	for(unsigned i = 0; i< 10; i++)
+	unsigned long long current= 0, previous = 0;
+
+	for(unsigned i = 0; i< 1000; i++)
 	{
 		Task_one();
 		Task_two();
+
+		current = RDTSC();
+
+		printf("Time difference time: %lld \n ", current - previous);
+
+		previous = current;
 	}
+}
+
+static inline volatile unsigned long long RDTSC(void) {
+   register unsigned long long TSC asm("eax");
+   asm volatile (".byte 15, 49" : : : "eax", "edx");
+   return TSC;
 }
 
 static void Task_one(void)
