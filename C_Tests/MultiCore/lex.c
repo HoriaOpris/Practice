dc5b3ca1d4950bfa2cfab7e1044b5690b21dd8fb
Horia Opris <horia.opris@outlook.com>
2018-10-13 21:07:06 +0100

    Implement factorial + remove math lib

--- C_Tests/MultiCore/lex.c
+++ C_Tests/MultiCore/lex.c
@@ -6,19 +6,42 @@ A permutation is an ordered arrangement of objects. For example, 3124 is one pos
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
-static unsigned digits[] = {3,1,2};
+static unsigned digits[] = {3, 1, 2};
+
+static unsigned PowerOfTen(unsigned power)
+{
+	const unsigned result[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+
+	if (power < (sizeof(result) / sizeof(result[0])))
+	{
+		return result[power];
+	}
+	else
+	{
+		printf("power error!\n");
+		return 0;
+	}
+}
+
+static unsigned Factorial(unsigned n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
 
 void main(void)
 {
-	unsigned res = 0;
+	unsigned res[6] = {0};
 
-	for(unsigned i = 0; i < 3; i++){
-		res += digits[i] * pow(10, i);
-	}
+	for (unsigned j = 0; j < Factorial(3); j++)
+		for (unsigned i = 0; i < 3; i++)
+		{
+			res[j] += digits[i] * PowerOfTen(i);
+		}
 
-	printf("%d\n", res);
+	printf("%d %d %d %d %d %d\n", res[0], res[1], res[2], res[3], res[4], res[5]);
 }
-
