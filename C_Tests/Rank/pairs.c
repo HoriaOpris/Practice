df48882d1ca6b3fd06d82612d5b7f9d49ad04856
Horia Opris <horia.opris@outlook.com>
2021-01-11 15:28:51 +0200

    Add pairs.c problem solving practice

---
+++ C_Tests/Rank/pairs.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//input line 1 number of items. line 2 space separated item color
+//output total number of color pairs
+
+int main (void)
+{
+    printf("test");
+
+    return 0;
+}
