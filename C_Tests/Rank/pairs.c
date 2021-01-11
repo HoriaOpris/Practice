b73d71fbab50118c6422f2b4683db4579261d8c7
Horia Opris <horia.opris@outlook.com>
2021-01-11 15:45:35 +0200

    Implement input get for first line (no of items)

--- C_Tests/Rank/pairs.c
+++ C_Tests/Rank/pairs.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 //input line 1 number of items. line 2 space separated item color
 //output total number of color pairs
 
+struct in
+{
+    unsigned items;
+    int *color;//array the size of items 
+};
+
+static struct in InputGet(void)
+{
+    struct in input;
+    scanf("%d", &input.items);
+    input.color = malloc(sizeof(int) * input.items);
+
+    input.color[1] = 3;
+
+    return input;
+}
+
 int main (void)
 {
-    printf("test");
+    struct in input = InputGet();
+
+    printf("in:%d val:%d\n", input.items, input.color[1]);
 
     return 0;
 }
