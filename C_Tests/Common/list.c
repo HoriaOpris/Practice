45bead4a168dce3cc38cc178eb288fe1182b89f0
Horia Opris <horia.opris@outlook.com>
2021-01-25 23:51:18 +0200

    Implement linked list type

--- C_Tests/Common/list.c
+++ C_Tests/Common/list.c
@@ -1,1 +1,32 @@
 //Implements list data type
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list
+{
+    int data;
+    struct list *next;
+};
+
+void Add(int data)
+{
+}
+
+int main(void)
+{
+    struct list l = {.data = 0, .next = NULL};
+
+    l.next = malloc(sizeof(struct list));
+    l.next->data = 1;
+
+    l.next->next = malloc(sizeof(struct list));
+    l.next->next->data = 2;
+
+    struct list *p = &l;
+    for(unsigned i = 0; i < 3; i++)
+    {
+        printf("data:%d pointer:%x\n", p->data, (unsigned)p->next);
+        p = p->next;
+    }
+
+}
