e7045c473267ae8146dc5072e553482c5d44314c
Horia Opris <horia.opris@outlook.com>
2021-01-26 00:07:10 +0200

    Implement linked list Add and Size functions

--- C_Tests/Common/list.c
+++ C_Tests/Common/list.c
@@ -8,25 +8,45 @@ struct list
     struct list *next;
 };
 
-void Add(int data)
+void Add(struct list *l, int data)
 {
+    struct list *p = l;
+
+    while (p->next != NULL)
+        p = p->next;
+
+    p->next = malloc(sizeof(struct list));
+    p->next->data = data;
+    p->next->next = NULL;
+}
+
+unsigned Size(struct list *l)
+{
+    struct list *p = l;
+    unsigned size = 1;
+
+    while (p->next != NULL)
+    {
+        size++;
+        p = p->next;
+    }
+
+    return size;
 }
 
 int main(void)
 {
     struct list l = {.data = 0, .next = NULL};
 
-    l.next = malloc(sizeof(struct list));
-    l.next->data = 1;
-
-    l.next->next = malloc(sizeof(struct list));
-    l.next->next->data = 2;
+    Add(&l, 1);
+    Add(&l, 2);
+    Add(&l, 3);
 
     struct list *p = &l;
-    for(unsigned i = 0; i < 3; i++)
+
+    for (unsigned i = 0; i < Size(&l); i++)
     {
         printf("data:%d pointer:%x\n", p->data, (unsigned)p->next);
         p = p->next;
     }
-
 }
