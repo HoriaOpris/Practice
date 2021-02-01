64c05b0752f627c3595bfe1cbf66babc3010b794
Horia Opris <horia.opris@outlook.com>
2021-02-01 22:34:02 +0200

    Implement bubble sort

--- C_Tests/AOC/aoc1.c
+++ C_Tests/AOC/aoc1.c
@@ -1,18 +1,39 @@
-#include<stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 #define ARR_SIZE(arr) sizeof(arr) / sizeof(arr[0])
 
 static unsigned input[] = {
-    #include"input.txt"
+#include "input.txt"
 };
 
-static unsigned *Sort(unsigned *arr, unsigned size)
+//Bubble
+static void Sort(unsigned *arr, unsigned size)
 {
-    unsigned *sorted = malloc(size);
+    while (1)
+    {
+        unsigned count;
 
+        for (unsigned i = 0; i < size - 1; i++)
+        {
+            unsigned temp;
 
-    return sorted;
+            if (arr[i] > arr[i + 1])
+            {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        if (count == (size - 1))
+            break;
+        else
+            count = 0;
+    }
 }
 
 static unsigned GetPairMultiplication(unsigned *arr, unsigned size)
@@ -22,10 +43,10 @@ static unsigned GetPairMultiplication(unsigned *arr, unsigned size)
 
 int main(void)
 {
-    unsigned *sorted = Sort(input, ARR_SIZE(input));
+    Sort(input, ARR_SIZE(input));
 
-    for(unsigned i = 0; i < ARR_SIZE(input); i++)
-        printf("%d\n", sorted[i]);
+    for (unsigned i = 0; i < ARR_SIZE(input); i++)
+        printf("%d\n", input[i]);
 
     return 0;
 }
