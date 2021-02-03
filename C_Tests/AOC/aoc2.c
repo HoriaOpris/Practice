885efa720f849c6617f10a89196d0475a4ee183c
Horia Opris <horia.opris@outlook.com>
2021-02-03 17:43:33 +0200

    Store all combinations in an array in order to sort later

--- C_Tests/AOC/aoc2.c
+++ C_Tests/AOC/aoc2.c
@@ -1,45 +1,55 @@
 //how many different ways can a number be written as a sum of at least 2 integers
 #include <stdio.h>
+static unsigned ways[10][10];
 
 int main(void)
 {
     const unsigned number = 5;
     unsigned sum = 0;
     unsigned ways_count = 0;
+    unsigned valid = 0;
+    unsigned col = 0;
 
     for (unsigned i = 1; i < number; i++)
     {
         for (unsigned j = 1; j < number; j++)
         {
             sum = i + j;
-            printf(" %d %d", i, j);
+            ways[valid][0] = i;
+            ways[valid][1] = j;
+            col = 2;
 
             while(1)
             {
                 if(sum > number)
                 {
-                    printf(" Fail");
                     break;
                 }
                 else if(sum == number)
                 {
-                    printf(" Correct");
+                    valid++;
                     ways_count++;
                     break;
                 }
                 else
                 {
-                    printf(" %d", i);
                     sum += i;
+                    ways[valid][col] = i;
+                    col++;
                 }
                 
             }
-
-            printf("\n");
         }
     }
 
-    printf("\nWays: %d\n", ways_count);
+    for(unsigned i = 0; i < valid; i++)
+    {
+        for(unsigned j = 0; j < 10; j++)
+        {
+            printf("%d ", ways[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
