739dc6ce5031bdd52eb551d9c301790ff8c1afd3
Horia Opris <horia.opris@outlook.com>
2021-03-20 19:05:27 +0000

    leet march init

--- C_Tests/AOC/aoc2.c
+++ C_Tests/AOC/aoc2.c
@@ -3,35 +3,35 @@
 #include "common.h"
 
 #define COL_SIZE 10
-#define ROW_SIZE 10
+#define ROW_SIZE 40
 
 static unsigned ways[COL_SIZE][ROW_SIZE];
 
 static void RemoveDuplicates(void)
 {
     unsigned cnt = 0;
-
-    for (unsigned i = 0; i < COL_SIZE; i++)
+    
+    for (unsigned i = 0; i < ROW_SIZE; i++)
     {
-        for (unsigned j = 0; j < COL_SIZE; j++)
+        for (unsigned j = 0; j < ROW_SIZE; j++)
         {
             cnt = 0;
-
+            
             if (i == j)
                 continue;
-
-            for (unsigned k = 0; k < ROW_SIZE; k++)
+            
+            for (unsigned k = 0; k < COL_SIZE; k++)
             {
                 if (ways[i][k] == ways[j][k])
                 {
                     cnt++;
                 }
             }
-
+            
             //delete line
-            if (cnt == ROW_SIZE)
+            if (cnt == COL_SIZE)
             {
-                for (unsigned l = 0; l < ROW_SIZE; l++)
+                for (unsigned l = 0; l < COL_SIZE; l++)
                 {
                     ways[j][l] = 0;
                 }
@@ -44,9 +44,9 @@ static unsigned CountWays(void)
 {
     unsigned ways_count = 0;
     
-    for(unsigned i = 0; i < COL_SIZE; i++)
+    for(unsigned i = 0; i < ROW_SIZE; i++)
     {
-        for(unsigned j = 0; j < ROW_SIZE; j++)
+        for(unsigned j = 0; j < COL_SIZE; j++)
         {
             if(ways[i][j] != 0)
             {
@@ -61,12 +61,12 @@ static unsigned CountWays(void)
 
 int main(void)
 {
-    const unsigned number = 5;
+    const unsigned number = 6;
     unsigned sum = 0;
     unsigned ways_count = 0;
     unsigned valid = 0;
     unsigned col = 0;
-
+    
     for (unsigned i = 1; i < number; i++)
     {
         for (unsigned j = 1; j < number; j++)
@@ -75,14 +75,14 @@ int main(void)
             ways[valid][0] = i;
             ways[valid][1] = j;
             col = 2;
-
+            
             while (1)
             {
                 if (sum > number)
                 {
-                    for(unsigned k = 0; k < ROW_SIZE; k++)
-                        ways[valid][k] = 0;
-                        break;
+                    for(unsigned k = 0; k < COL_SIZE; k++)
+                    ways[valid][k] = 0;
+                    break;
                 }
                 else if (sum == number)
                 {
@@ -99,23 +99,24 @@ int main(void)
             }
         }
     }
-
+    
     for (unsigned i = 0; i < valid; i++)
     {
-        Sort(&ways[i][0], ROW_SIZE);
+        Sort(&ways[i][0], COL_SIZE);
     }
-
+    
     RemoveDuplicates();
     printf("Ways: %d \n", CountWays());
     
     for (unsigned i = 0; i < valid; i++)
     {
-        for (unsigned j = 0; j < ROW_SIZE; j++)
+        for (unsigned j = 0; j < COL_SIZE; j++)
         {
-            printf("%d ", ways[i][j]);
+            if(ways[i][j]!=0)
+                printf("%d ", ways[i][j]);
         }
         printf("\n");
     }
-
+    
     return 0;
 }
---
+++ LeetCode/LeetCode/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Private Privacy on 20/03/2021.
+//
+
+import Foundation
+
+class Solution {
+    func distributeCandies(candyType: [Int]) -> Int {
+        return 1
+    }
+}
+
+var sol:Solution = Solution()
+
+print(sol.distributeCandies(candyType: [1,2,3]))
