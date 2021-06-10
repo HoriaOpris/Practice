dbba854b960852d56ff230425a47afe5f4cadbcb
Horia Opris <horia.opris@outlook.com>
2021-06-10 22:33:42 +0100

    Implement lock using atomic test and set

--- OS/Preemptive/shared.c
+++ OS/Preemptive/shared.c
@@ -2,22 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h> //Header file for sleep(). man 3 sleep for details.
 #include <pthread.h>
+#include <stdatomic.h>
 
+//_Atomic int shared = 0;
 int shared = 0;
-unsigned lock = 0;
+int integrity = ~0;
+atomic_flag lock = ATOMIC_FLAG_INIT;
 pthread_mutex_t mutex;
 
 void *Thread0()
 {
     for (int i = 0; i < 10000000; i++)
     {
-        //while (lock == 1)
-        //    ;
-        pthread_mutex_lock(&mutex);
-        //lock = 1;
-        shared++;
-        //lock = 0;
-        pthread_mutex_unlock(&mutex);
+        while (atomic_flag_test_and_set(&lock))
+            ;
+        //pthread_mutex_lock(&mutex);
+
+        if (shared == ~integrity)
+        {
+            shared++;
+            integrity = ~shared;
+        }
+        else
+        {
+            printf("\n fail ");
+            break;
+        }
+
+        atomic_flag_clear(&lock);
+        //pthread_mutex_unlock(&mutex);
     }
 
     return NULL;
