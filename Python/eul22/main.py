c688d9cd032491f8315b8a6ac0ede371e6302b02
Horia Opris <horia.opris@outlook.com>
2023-01-20 17:11:26 +0000

    Calculate total name scores

--- Python/eul22/main.py
+++ Python/eul22/main.py
@@ -1,3 +1,4 @@
+from alphabet import Alphabet
 f = open("names.txt", "r")
 
 names_raw = f.readline()
@@ -17,11 +18,19 @@ for character in names_raw:
 # Sort names
 names.sort()
 
+# What is the total of all the name scores in the file?
+alphabet = Alphabet()
+
+total_score = 0
 name_position_in_list = 0
 for name in names:
+    name_score = 0
     name_position_in_list += 1
-    # print(name)
 
-    if name == "COLIN":
-        print(name + " " + str(name_position_in_list) + " " +
-              str(ord(name[0])) + " " + str(ord(name[1])))
+    for character in name:
+        name_score += alphabet.getIndexOfLetter(character)
+
+    name_score *= name_position_in_list
+    total_score += name_score
+
+print("Total name scores: " + str(total_score))
