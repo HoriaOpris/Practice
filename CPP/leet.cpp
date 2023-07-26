24b274e4740e1316aee61a8b2f51115bf5a94eaf
Horia Opris <horia.opris@outlook.com>
2023-07-26 18:53:12 +0100

    Add water containter area calculator basic

--- CPP/leet.cpp
+++ CPP/leet.cpp
@@ -21,16 +21,43 @@ public:
     }
 };
 
+class WaterContainer
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int max = 0;
+
+        for (int i = 0; i < height.size(); i++)
+        {
+            for (int j = 0; j < height.size(); j++)
+            {
+                int length = j - i;
+                int water_height = (height[i] < height[j]) ? height[i] : height[j];
+                int current_area = length * water_height;
+
+                if (current_area > max)
+                {
+                    max = current_area;
+                }
+            }
+        }
+
+        return max;
+    }
+};
+
 int main()
 {
     vector<int> v1 = {1, 1, 2};
     vector<int> v2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-
     Leet22 d;
     cout << "unique elements v1: " << d.removeDuplicates(v1) << endl;
     cout << "unique elements v2: " << d.removeDuplicates(v2) << endl;
 
-    
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    WaterContainer Water;
+    cout << "Max water area: " << Water.maxArea(height) << endl;
 
     return 0;
 }
