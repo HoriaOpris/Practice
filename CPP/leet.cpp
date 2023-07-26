881e35046082b2b4a73afb330cec5778b226b8d0
Horia Opris <horia.opris@outlook.com>
2023-07-26 22:47:22 +0100

    Add sudoku validation

--- CPP/leet.cpp
+++ CPP/leet.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -39,7 +40,7 @@ public:
                 max = current_area;
             }
 
-            if(height[i] < height[j])
+            if (height[i] < height[j])
             {
                 i++;
             }
@@ -50,6 +51,81 @@ public:
         }
 
         return max;
+    }
+};
+
+class ValidSudoku
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        // check rows
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                for (int k = j + 1; k < 9; k++)
+                {
+                    if (board[i][j] != '.')
+                    {
+                        if (board[i][j] == board[i][k])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // check columns
+        for (int col = 0; col < 9; col++)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int k = row + 1; k < 9; k++)
+                {
+                    if (board[row][col] != '.')
+                    {
+                        if (board[row][col] == board[k][col])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        cout << endl;
+        // sub_box
+        vector<vector<char>> sub_box = board;
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                sub_box[i][j] = board[((i / 3) * 3) + (j / 3)][(j % 3) + (3 * (i % 3))];
+            }
+            cout << endl;
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                for (int k = j + 1; k < 9; k++)
+                {
+                    if (sub_box[i][j] != '.')
+                    {
+                        if (sub_box[i][j] == sub_box[i][k])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 };
 
@@ -64,6 +140,23 @@ int main()
     vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
     WaterContainer Water;
     cout << "Max water area: " << Water.maxArea(height) << endl;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    vector<vector<char>> board =
+        {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+         {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+         {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+         {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+         {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+         {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+         {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+         {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+         {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    ValidSudoku Sudoku;
+    cout << "Sudoku: " << Sudoku.isValidSudoku(board) << endl;
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> ms_double = t2 - t1;
+    std::cout << ms_double.count() << "ms\n";
 
     return 0;
 }
